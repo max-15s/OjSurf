63b5c515c6026a808875c33ffcb81d1bde109c37
Amin Niazi <niazi@rahkar.co>
2021-06-04 01:16:39 +0430

    Quera/3537

---
+++ Quera/3537.py
@@ -0,0 +1,6 @@
+# https://quera.ir/problemset/contest/3537/%D8%B3%D8%A4%D8%A7%D9%84-%D9%BE%DB%8C%D8%A7%D8%AF%D9%87-%D8%B3%D8%A7%D8%B2%DB%8C-%D8%B3%D9%88%D8%A7%D9%84-%D8%B2%D8%B1%D8%AF
+
+if __name__ == '__main__':
+    n = int(input())
+    sub = 'o' * n
+    print(f'W{sub}w!')
