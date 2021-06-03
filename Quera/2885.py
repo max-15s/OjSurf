34eac3d63e0f0e0b909916f0c58bc550a429a2ec
Amin Niazi <niazi@rahkar.co>
2021-06-04 01:22:34 +0430

    Quera/2885

---
+++ Quera/2885.py
@@ -0,0 +1,6 @@
+# https://quera.ir/problemset/contest/2885
+
+if __name__ == '__main__':
+    n = int(input())
+    sub = 'man khoshghlab hastam\n'*n
+    print(sub, end='')
--- Quera/3537.py
+++ Quera/3537.py
@@ -1,4 +1,4 @@
-# https://quera.ir/problemset/contest/3537/%D8%B3%D8%A4%D8%A7%D9%84-%D9%BE%DB%8C%D8%A7%D8%AF%D9%87-%D8%B3%D8%A7%D8%B2%DB%8C-%D8%B3%D9%88%D8%A7%D9%84-%D8%B2%D8%B1%D8%AF
+# https://quera.ir/problemset/contest/3537
 
 if __name__ == '__main__':
     n = int(input())
